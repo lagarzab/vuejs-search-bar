bdf7eaf3a464dd9750c6996580bcd20ecf430cd3
Bryan <lagarzab@gmail.com>
2020-04-20 14:35:50 -0400

    Cleaning up css

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -25,76 +25,60 @@ export default {
         flex:1;
     }
     .search-bar {
-        --size: 30px;
+        --size: 32px;
+        --font-size: max(1.25rem, calc(var(--size) / 1.6));
 
         border: 2px solid black;
         display: flex;
         margin: .5em;
-        border-radius: calc(var(--size));
+        border-radius: var(--size);
         height: var(--size);
         width: var(--size);
         flex: 0;
         flex-basis: initial;
         position:relative;
-        //transition: width 300ms cubic-bezier(0.18, 0.89, 0.32, 1.08);
         transition: flex 300ms cubic-bezier(0.18, 0.89, 0.32, 1.08);
-        z-index: 9;
+        z-index: 0;
 
         &__input {
             margin-left: auto;
             flex-grow: 1;
-            font-size: 1.5em;
+            font-size: var(--font-size);
             border: none;
             padding: 0 .5em;
             overflow: hidden;
             position:absolute;
+            height:100%;
+            width: 100%;
             line-height: 1;
             cursor: pointer;
-            background-color:rgba(0,0,0,.0);
+            background-color:rgba(0,0,0,1);
             opacity: 0;
-            width: calc(100% - var(--size));
-            z-index: -1;
-            border-radius: calc(var(--size));
+            z-index: 1;
+            border-radius: var(--size);
         }
 
         &__button {
-            --font-size: 1em;
+            position:relative;
             font-size: var(--font-size);
             border-radius: 50%;
-            margin-left:auto;
             height: calc(var(--size) - 10px);
             width: calc(var(--size) - 10px);
             border: none;
-            background:transparent;
             background-color:white;
-            transition: margin-left 300ms ease-in-out;
-
+            margin-left:auto;
             margin-top:auto;
             margin-bottom: auto;
             margin-right:3px;
-            justify-content: center;
-            align-content:center;
-            vertical-align: middle;
-            text-align: center;
-            position:relative;
-            //padding: calc(var(--font-size) / var(--font-size));
+            z-index: -1;
+            transition: background-color 200ms ease-in-out;
         }
 
         &__icon {
-            //display:block;
             position:absolute;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            // vertical-align: middle;
-            // justify-content: center;
-            // text-align: center;
-            // height:var(--size);
-            // width:var(--size);
-            //height: 100%;
-            //width:  100%;
-            //padding: 10px;
-            //padding-right: calc(var(--size) / 1em)
         }
 
         &:focus-within {
@@ -105,11 +89,13 @@ export default {
                 opacity: 1;
                 background-color: white;
                 cursor: initial;
+                padding-right: var(--size);
 
             }
             .search-bar__button {
                 background-color: black;
                 color: white;
+                z-index: 2;
             }
         }
     }
